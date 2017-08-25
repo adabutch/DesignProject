fc38e14ab5782854551fcce3273b3cbad05f8cbf
Adam Butcher <adabutch1@gmail.com>
2017-08-24 20:25:56 -0400

    additional gulp tools for min css & rename & pkg infos

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
-var gulp     =    require('gulp');
-var less     =    require('gulp-less');
-var watch    =    require('gulp-watch');
-var notify   =    require("gulp-notify");
-var plumber  =    require('gulp-plumber');
+var gulp       =    require('gulp');
+var less       =    require('gulp-less');
+var watch      =    require('gulp-watch');
+var notify     =    require("gulp-notify");
+var plumber    =    require('gulp-plumber');
+var cleanCSS   =    require('gulp-clean-css');
+var rename     =    require('gulp-rename');
+var header     =    require('gulp-header');
+var pkg        =    require('./package.json');
+
+/* Prepare banner text */
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * Author: <%= pkg.author.name %>',
+  ' * Email: <%= pkg.author.email %>',
+  ' * URL: <%= pkg.author.url %>',
+  ' * v<%= pkg.version %>',
+  ' */',
+  ''].join('\n');
 
 var onError = function (err) {
   notify({
@@ -25,7 +39,16 @@ gulp.task('less', function () {
       title   : "Gulp'd Less",
       message : 'Styles have been compiled'
     })
+    .pipe(header(banner, {pkg: pkg}))
   );
+});
+
+gulp.task('minify-css', function(){
+  return gulp.src('app/css/main.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('app/css/'));
 });
 
 gulp.task('watch', function() {
@@ -33,3 +56,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('build', ['minify-css'])
